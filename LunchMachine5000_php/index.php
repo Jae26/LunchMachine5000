25a777ff7d6c12a6c1d5d9b1d3a8a1e11aa602c9
Jae26 <38301190+Jae26@users.noreply.github.com>
2018-06-08 15:48:42 +0300

    changed 'Ravfla' to 'Rafla'

--- LunchMachine5000_php/index.php
+++ LunchMachine5000_php/index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Ravfla Picker "Lunch Machine 5000"</title>
+    <title>Rafla Picker "Lunch Machine 5000"</title>
     <link rel="shortcut icon" type="image/x-icon" href="k-logo.png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
