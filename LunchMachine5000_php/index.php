6a90d56e3042fe0bf67f4b59d0bc9d169d5a5d77
Jae26 <38301190+Jae26@users.noreply.github.com>
2018-06-12 15:29:39 +0300

    optimizing styles for mobile usage-lots of code changes

--- LunchMachine5000_php/index.php
+++ LunchMachine5000_php/index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="Lunchin_style.css">
+    <link rel="stylesheet" type="text/css" href="Lunchin_style-new.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -67,15 +67,13 @@
 		
 			<!--<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>-->
 		
-	<script>
+	<script >
 		function openNav() {
-			document.getElementById("myDropdown").style.width = "35%";
-			document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+			document.getElementById("myDropdown").style.width = "100%";
 		}
 
 		function closeNav() {
 			document.getElementById("myDropdown").style.width = "0";
-			document.body.style.backgroundColor = "white";
 		}
 	</script>
 	
