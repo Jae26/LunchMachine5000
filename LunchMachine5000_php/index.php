a0eed1485784ca6021380819f08959c982c5996b
Jae26 <38301190+Jae26@users.noreply.github.com>
2018-06-14 16:52:10 +0300

    minor changes to menu functionality, slight problem with mobile version (portrait) menu, will adjust and prepare for Pe 15.6

--- LunchMachine5000_php/index.php
+++ LunchMachine5000_php/index.php
@@ -23,57 +23,64 @@
     </script>
 </head>
 <body>
-<div class="wrapper">
-    <header class="header-container">
-     <!--Begin Drop Menu-->	
-    <div class="headNav">
-        <div class="containerT" onclick="openNav()"> <!--onclick="myFunction(this)">-->
-            <div id="barMenu">
-                <div class="bar1"></div>
-                <div class="bar2"></div>
-                <div class="bar3"></div>
+    <div id="wrapper">
+        <header id="menu-primary" class="menu">
+        <!--Begin Drop Menu-->	
+            <div class="menu-container">
+                <div id="menu-toggle-primary" class="containerT" onclick="openNav()"> <!--onclick="myFunction(this)">-->
+                    <div id="barMenu">
+                        <div class="bar1"></div>
+                        <div class="bar2"></div>
+                        <div class="bar3"></div>              
+                        <!--<div id="myDropdown" class="dropdown-content-hide">
+                            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">
+                                <div class="bar1a"></div>
+                                <div class="bar2a"></div>
+                                <div class="bar3a"></div>
+                            </a>
+                            <div id="myAdd">
+                                <a class="form-enter" href="create.php">Add</a>
+                            </div>       
+                            <a id="myEdit" class="form-enter" href="list.php">Edit</a>
+                        </div>-->
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    <div id="myDropdown" class="dropdown-content-hide"> <!--id="mySidenav" class="sidenav">-->
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">
-            <div class="bar1a"></div>
-            <div class="bar2a"></div>
-            <div class="bar3a"></div>
-        </a>
-        <!--&times;</a>-->
-        
-        <div id="myAdd">
-            <a class="form-enter" href="create.php">Add</a>
-        </div>          
-            
-            <a id="myEdit" class="form-enter" href="list.php">Edit</a>
-            
-    </div>	
-	<script >
-		function openNav() {
-			document.getElementById("myDropdown").style.width = "25%";            
-		}
-		function closeNav() {
-			document.getElementById("myDropdown").style.width = "0";
-		}
-    </script>
-    <!--End Drop Menu-->
-    </header>
-        
+            <div id="myDropdown" class="dropdown-content-hide"> <!--id="mySidenav" class="sidenav">-->
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">
+                    <div class="bar1a"></div>
+                    <div class="bar2a"></div>
+                    <div class="bar3a"></div>
+                </a>
+                <!--&times;</a>-->            
+                <div id="myAdd">
+                    <a class="form-enter" href="create.php">Add</a>
+                </div>          
+                    <a id="myEdit" class="form-enter" href="list.php">Edit</a>
+            </div>
+        <script >
+            function openNav() {
+                document.getElementById("myDropdown").style.width = "25%";            
+            }
+            function closeNav() {
+                document.getElementById("myDropdown").style.width = "0";
+            }
+        </script>
+        <!--End Drop Menu-->
+        </header>            
         <div id="container-fluidF" class="container-fluid">
             <div class="row">
                 <div class="col-md-12">                    
                         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
                             <script>
-                               $(document).ready(function() {
-                                  $("#getrestaurant").click(function(event){
-                                      $.get('getrestaurant.php',function(return_data){
-                                      $("#display").html(return_data); 
-                                      
-                                  });
-                                 });
-                               });
+                            $(document).ready(function() {
+                                $("#getrestaurant").click(function(event){
+                                    $.get('getrestaurant.php',function(return_data){
+                                    $("#display").html(return_data); 
+                                    
+                                });
+                                });
+                            });
                             </script>
                             <br>
                         <div class="text-center" id="display"></div>
@@ -83,30 +90,30 @@
                 </div>
             </div>        
         </div>  
-		
-		<!--<div id="logoArea">
-			
-		</div>-->
-    <footer class="footer">			
-		<div class="logo">
-			<img src="img/Kuulogo.png" alt="Kuuasema Logo">
-		</div>			
-        <div class="container">
-            <div class="text-center">
-                <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                    <label class="btn btn-secondary active">
-                        <input type="radio" name="options" id="option1" autocomplete="off" checked> Visits today
-                    </label>
-                        <label class="btn btn-secondary">
-                            <input type="radio" name="options" id="option2" autocomplete="off"> Visits total
+            
+            <!--<div id="logoArea">
+                
+            </div>-->
+        <footer class="footer">			
+            <div class="logo">
+                <img src="img/Kuulogo.png" alt="Kuuasema Logo">
+            </div>			
+            <div class="container">
+                <div class="text-center">
+                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                        <label class="btn btn-secondary active">
+                            <input type="radio" name="options" id="option1" autocomplete="off" checked> Visits today
                         </label>
+                            <label class="btn btn-secondary">
+                                <input type="radio" name="options" id="option2" autocomplete="off"> Visits total
+                            </label>
+                    </div>
+                        <br>
+                        <span class="text-muted">00000000</span>
                 </div>
-                    <br>
-                    <span class="text-muted">00000000</span>
             </div>
-        </div>
-    </footer>
-</div>
+        </footer>
+    </div>
 </body>
 </html>
 
